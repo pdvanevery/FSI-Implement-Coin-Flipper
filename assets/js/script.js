05499043360d9c549df18b84cf56f9ede903425c
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2021-11-17 21:21:26 -0800

    Updated HTML and Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // TODO: Declare any global variables we need
 
+let totalTails = 0;
+let totalHeads = 0;
+
+let coinImg = document.getElementById('penny-heads');
+let total = totalHeads + totalTails;
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
@@ -7,6 +13,41 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+
+    document.getElementById('flip-button').addEventListener('click', () => {
+        // If 0.5 or less = tails, else = heads
+        let result = Math.random() < 0.5;
+        //
+        if (result < 0.5) {
+            coinImg.src = './assets/images/penny-heads.jpg';
+
+            totalHeads = totalHeads + 1;
+        } else {
+            coinImg.src = './assets/images/penny-tails.jpg';
+
+            totalTails = totalTails + 1;
+        }
+        //Update the scoreboard
+        let total = totalHeads + totalTails;
+        
+        let percentTails = 0;
+        let percentHeads = 0;
+
+
+        if (total > 0) {
+            //Calculate the percents of heads and tails
+            percentHeads = Math.round((totalHeads / total) * 100);
+            percentTails = Math.round((totalTails / total) * 100);
+        }
+
+        //Update the scoreboard values
+        document.getElementById('heads').textContent = totalHeads;
+        document.getElementById('heads-percent').textContent = percentHeads + '%';
+        document.getElementById('tails').textContent = totalTails;
+        document.getElementById('tails-percent').textContent = percentTails + '%';
+
+
+    });
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
